d0f455f93d37ea852658899297a866f00fda0a80
Xander Dwight <martindwyte@gmail.com>
2025-09-16 22:48:53 +0800

    fix: fixed Anchore styled components warnings

--- app/components/A/A.tsx
+++ app/components/A/A.tsx
@@ -14,9 +14,9 @@ export function A({ children, ...rest }: React.ComponentPropsWithoutRef<"a">) {
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className="lucide lucide-arrow-up-right-icon lucide-arrow-up-right inline"
       >
         <path d="M7 7h10v10" />
