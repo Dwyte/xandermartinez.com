05d52e5ea2c77571e1d3a9e7cc95bf4086baafc2
Xander Dwight <martindwyte@gmail.com>
2025-09-16 21:03:34 +0800

    chore: add contact urls on constants.ts

--- app/constants.ts
+++ app/constants.ts
@@ -1,1 +1,4 @@
 export const TITLE = "Xander Martinez";
+export const EMAIL_URL = "https://www.gmail.com";
+export const GITHUB_URL = "https://www.github.com";
+export const LINKEDIN_URL = "https://www.linkedin.com";
