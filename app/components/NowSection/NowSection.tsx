75e4b1447e7a64b9fd86d76fea4a14ad44e155af
Xander Dwight <martindwyte@gmail.com>
2025-09-16 23:58:59 +0800

    feat: add NowSection boilerplate

---
+++ app/components/NowSection/NowSection.tsx
@@ -0,0 +1,15 @@
+import { NumberedHeader } from "../NumberedHeader";
+
+export function NowSection() {
+  return (
+    <div className="w-[100%] max-w-2xl flex flex-col gap-4">
+      <NumberedHeader number="11" content="Now" />
+      <p className="text-base text-gray-500">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci velit
+        odio error impedit debitis molestiae quidem molestias iste dolor
+        sapiente tempora cumque sunt at consectetur aliquid eveniet, quasi
+        pariatur cum!
+      </p>
+    </div>
+  );
+}
