78fa32d4fc5d39c137d9733ab5c227a41a5ee92d
Xander Dwight <martindwyte@gmail.com>
2025-09-16 21:05:22 +0800

    feat: add HeroSection component

---
+++ app/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,19 @@
+import { TITLE, EMAIL_URL, LINKEDIN_URL, GITHUB_URL } from "@/app/constants";
+import { A } from "../A";
+
+export function HeroSection() {
+  return (
+    <div className="max-w-2xl flex flex-col gap-2">
+      <h1 className="text-lg text-gray-500">{TITLE}</h1>
+      <p className="text-2xl">
+        Developing thoughtful web experiences — with precision, maintainability,
+        and performance.
+      </p>
+      <p className="text-lg text-gray-500">
+        Get in touch via <A href={EMAIL_URL}>Email</A> or{" "}
+        <A href={LINKEDIN_URL}>LinkedIn</A>, see my code on{" "}
+        <A href={GITHUB_URL}>Github</A>.
+      </p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-import { TITLE } from "./constants";
+import { HeroSection } from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center px-8">
-      <div className="max-w-2xl pt-32 flex flex-col gap-2">
-        <h1 className="text-lg text-gray-500">{TITLE}</h1>
-        <p className="text-2xl">
-          Developing thoughtful web experiences — with precision,
-          maintainability, and performance.
-        </p>
-        <p className="text-lg text-gray-500">
-          Get in touch via Email or LinkedIn, see my code on Github.
-        </p>
-      </div>
+    <div className="flex items-center  pt-32 justify-center px-8">
+      <HeroSection />
     </div>
   );
 }
