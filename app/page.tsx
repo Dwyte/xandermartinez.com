0fcd5b6acd9ce714a95475c061c1f567deb52fba
Xander Dwight <martindwyte@gmail.com>
2025-09-16 23:59:26 +0800

    style: add larger y-padding not only top on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { HeroSection } from "./components/HeroSection";
+import { NowSection } from "./components/NowSection";
+import { ProjectsSection } from "./components/ProjectsSection";
 import { WorkSection } from "./components/WorkSection";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center pt-32 px-8 gap-12">
+    <div className="flex flex-col items-center py-42 px-8 gap-12">
       <HeroSection />
       <WorkSection />
+      <ProjectsSection />
+      <NowSection />
     </div>
   );
 }
