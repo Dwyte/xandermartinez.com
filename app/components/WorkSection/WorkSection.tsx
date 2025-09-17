d070075d831e7b75d3c05ddee1d215ab1f31262e
Xander Dwight <martindwyte@gmail.com>
2025-09-17 21:37:19 +0800

    feat: add work date year info on WorkSection

--- app/components/WorkSection/WorkSection.tsx
+++ app/components/WorkSection/WorkSection.tsx
@@ -2,6 +2,32 @@ import { WORK_EXPRIENCES } from "@/app/constants";
 import { NumberedHeader } from "../NumberedHeader";
 import { List, ListItem } from "../List";
 
+export function WorkItem({ work }: { work: WorkExperience }) {
+  const fromDateStr = work.fromDate.toLocaleDateString("en-US", {
+    year: "numeric",
+  });
+
+  const toDateStr =
+    typeof work.toDate === "string"
+      ? work.toDate
+      : work.toDate.toLocaleDateString("en-US", {
+          year: "numeric",
+        });
+
+  return (
+    <div className="group/work-item flex flex-col gap-1">
+      <div className="flex items-center justify-between gap-2 flex-row">
+        <h3 className="text-lg font-semibold">{work.company}</h3>
+        <p className="mt-1 opacity-0 text-xs group-hover/work-item:opacity-100 transition-opacity">
+          {fromDateStr} – {toDateStr}
+        </p>
+      </div>
+
+      <p className="text-base">{work.summary}</p>
+    </div>
+  );
+}
+
 export function WorkSection() {
   return (
     <div className="section">
@@ -9,13 +35,7 @@ export function WorkSection() {
       <List className="flex flex-col gap-4">
         {WORK_EXPRIENCES.map((work) => (
           <ListItem key={work.id}>
-            <div className="flex flex-col gap-2">
-              <h3 className="text-xl font-semibold">
-                {work.position} · {work.company}
-              </h3>
-
-              <p className="text-base">{work.summary}</p>
-            </div>
+            <WorkItem work={work} />
           </ListItem>
         ))}
       </List>
