721519f09e50347ac11cff63e30138e854b7040e
Xander Dwight <martindwyte@gmail.com>
2025-09-19 21:20:33 +0800

    feat: add backwards for StyledLink

--- app/components/StyledLink/StyledLink.tsx
+++ app/components/StyledLink/StyledLink.tsx
@@ -1,26 +1,43 @@
 import clsx from "clsx";
-import { ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import Link, { LinkProps } from "next/link";
 
 type StyledLinkProps = LinkProps & {
   children?: React.ReactNode;
   className?: string;
+  backwards?: boolean;
 };
 
-export function StyledLink({ children, className, ...rest }: StyledLinkProps) {
+export function StyledLink({
+  children,
+  className,
+  backwards,
+  ...rest
+}: StyledLinkProps) {
   return (
     <Link
       {...rest}
       className={clsx("group/styled-link link hover:text-gray-200", className)}
     >
+      {backwards && (
+        <ArrowLeft
+          stroke="currentColor"
+          strokeWidth={2}
+          width="1em"
+          className="group-hover/styled-link:translate-x-[-30%] transition-transform"
+        />
+      )}
+
       {children}
 
-      <ArrowRight
-        stroke="currentColor"
-        strokeWidth={2}
-        width="1em"
-        className="group-hover/styled-link:translate-x-[30%] transition-transform"
-      />
+      {!backwards && (
+        <ArrowRight
+          stroke="currentColor"
+          strokeWidth={2}
+          width="1em"
+          className="group-hover/styled-link:translate-x-[30%] transition-transform"
+        />
+      )}
     </Link>
   );
 }
