92eb0ec11a1e4e76ea3e804f84c2a823acab2324
Xander Dwight <martindwyte@gmail.com>
2025-09-20 04:07:10 +0800

    style: brighter numberedheader

--- app/components/NumberedHeader/NumberedHeader.tsx
+++ app/components/NumberedHeader/NumberedHeader.tsx
@@ -9,7 +9,9 @@ export function NumberedHeader({ number, content }: NumberedHeaderProps) {
       <span className="absolute z-[-1] left-[-42] top-[25%] translate-[-50%] translate-x-[-50%] text-7xl font-mono bg-clip-text text-transparent bg-linear-to-l from-gray-900 via-50% via-gray-900/30 to-gray-900/0">
         {number.toString(2).padStart(4, "0")}
       </span>
-      <span className="text-xl static">{content}</span>
+      <span className="text-xl static text-gray-700 brightness-125">
+        {content}
+      </span>
     </h2>
   );
 }
