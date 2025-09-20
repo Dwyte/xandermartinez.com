33a73652aaf5837496ba096ff5c191399b524dad
Xander Dwight <martindwyte@gmail.com>
2025-09-20 22:42:34 +0800

    feat: add actual content for now section

--- app/components/NowSection/NowSection.tsx
+++ app/components/NowSection/NowSection.tsx
@@ -1,15 +1,57 @@
+import { A } from "../A";
+import { HoverReveal } from "../hover-reveal";
 import { NumberedHeader } from "../NumberedHeader";
 
 export function NowSection() {
   return (
-    <div className="section">
+    <section className="section leading-relaxed text-gray-600 [&_strong]:font-normal [&_strong]:text-gray-100">
       <NumberedHeader number={3} content="Now" />
-      <p className="text-base text-gray-500">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci velit
-        odio error impedit debitis molestiae quidem molestias iste dolor
-        sapiente tempora cumque sunt at consectetur aliquid eveniet, quasi
-        pariatur cum!
-      </p>
-    </div>
+      <div className="[&>p]:mb-4">
+        <p>
+          Running my own business taught me valuable lessons, but it also
+          reminded me where I do my best work—
+          <span className="text-gray-400">
+            &nbsp;creating solutions through software.
+          </span>
+        </p>
+        <p>
+          Now,{" "}
+          <strong>
+            I&apos;m piecing things together again, focused on getting back into
+            tech
+          </strong>
+          ,{" "}
+          <HoverReveal>
+            and crossing my fingers I&apos;m not too late in this AI-driven
+            world.
+          </HoverReveal>{" "}
+          <strong>
+            Showing up daily: refining my fundamentals and exploring new
+            technologies. Focused on what matters,{" "}
+            <A
+              href="https://jamesclear.com/continuous-improvemena"
+              className="inline"
+            >
+              improving 1% each day
+            </A>
+          </strong>
+          ,{" "}
+          <HoverReveal>
+            and reminding myself of this whenever I feel behind.
+          </HoverReveal>
+        </p>
+        <p>
+          When I&apos;m AFK, I&apos;m learning balance by being physically
+          active, investing in the people I care about, and treating my health
+          as seriously as my work.
+        </p>
+        <p>
+          <strong>
+            Along the way, I&apos;m open to opportunities where I can learn,
+            grow, and contribute.
+          </strong>
+        </p>
+      </div>
+    </section>
   );
 }
