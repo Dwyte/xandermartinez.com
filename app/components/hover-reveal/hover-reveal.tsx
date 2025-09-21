a84410baeb1b4e2f5b25f0ec359f12d4d087292d
Xander Dwight <martindwyte@gmail.com>
2025-09-21 17:40:07 +0800

    feat: make hover-reveal work on mobile

--- app/components/hover-reveal/hover-reveal.tsx
+++ app/components/hover-reveal/hover-reveal.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import clsx from "clsx";
 
 type HoverRevealProps = React.ComponentPropsWithoutRef<"span">;
@@ -7,9 +10,17 @@ export function HoverReveal({
   className,
   ...rest
 }: HoverRevealProps) {
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <span
-      className={clsx("blur-sm hover:blur-none transition-all", className)}
+      onMouseEnter={() => setIsActive(true)}
+      onMouseLeave={() => setIsActive(false)}
+      className={clsx(
+        "transition-all",
+        isActive ? "blur-none" : "blur-sm",
+        className
+      )}
       {...rest}
     >
       {children}
