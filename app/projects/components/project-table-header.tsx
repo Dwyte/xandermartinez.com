4baa7267299b646437f125a8e8813f3c5d79a64e
Xander Dwight <martindwyte@gmail.com>
2025-09-21 20:49:04 +0800

    style: put header toggle button on project-table-header inside div so it doesn't stretch the button itself.

--- app/projects/components/project-table-header.tsx
+++ app/projects/components/project-table-header.tsx
@@ -13,16 +13,22 @@ export function ProjectTableHeader({
     <div className="flex gap-2 pb-2 pr-2 font-semibold text-gray-700">
       <div className="min-w-9 mr-14">Year</div>
       <div className="flex-1 md:max-w-75 md:min-w-75">Project</div>
-      <button
-        onClick={toggleThirdColumn}
-        className="flex-1 hidden md:block text-left cursor-pointer hover:brightness-150 transition-all"
-      >
-        {isShowSummary ? "About" : "Built with"}{" "}
-        <ArrowLeftRight stroke="currentColor" width="1em" className="inline" />{" "}
-        <span className="text-gray-800">
-          {isShowSummary ? "Built with" : "About"}
-        </span>
-      </button>
+      <div className="flex-1 hidden md:block text-left">
+        <button
+          onClick={toggleThirdColumn}
+          className="cursor-pointer hover:brightness-150 transition-all"
+        >
+          {isShowSummary ? "About" : "Built with"}{" "}
+          <ArrowLeftRight
+            stroke="currentColor"
+            width="1em"
+            className="inline"
+          />{" "}
+          <span className="text-gray-800">
+            {isShowSummary ? "Built with" : "About"}
+          </span>
+        </button>
+      </div>
       <div className="hidden sm:block">Links</div>
     </div>
   );
