168ac6cb7b159cbea8547d495690b9c90b0b76a9
Xander Dwight <martindwyte@gmail.com>
2025-09-22 22:45:16 +0800

    fix: hydration warnings on footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,16 +1,21 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { QUOTES, TITLE } from "@/app/constants";
 
 export function Footer() {
+  const [quote, setQuote] = useState<null | string>(null);
+
+  useEffect(() => {
+    setQuote(QUOTES[Math.floor(Math.random() * QUOTES.length)]);
+  }, []);
+
   return (
     <footer className="w-full flex flex-col items-center justify-start">
       <div className="h-[1px] w-full max-w-6xl bg-linear-to-r from-gray-700/0 via-50% via-gray-100/50 to-gray-700/0"></div>
       <div className="w-full max-w-4xl flex justify-between text-gray-600 py-6 opacity-75">
         <strong className="font-serif leading-none">{TITLE}</strong>
-        <strong className="font-serif leading-none">
-          {QUOTES[Math.floor(Math.random() * QUOTES.length)]}
-        </strong>
+        <strong className="font-serif leading-none">{quote}</strong>
       </div>
     </footer>
   );
