25eb42ceefeb9c5614a40759ce8bdc18b23f69fc
Xander Dwight <martindwyte@gmail.com>
2025-10-02 01:07:40 +0800

    style: change font style in numbered-headers from sans-serif to serif

--- app/components/numbered-header/numbered-header.tsx
+++ app/components/numbered-header/numbered-header.tsx
@@ -7,11 +7,11 @@ const roman = ["I", "II", "III", "IV"];
 
 export function NumberedHeader({ number, content }: NumberedHeaderProps) {
   return (
-    <h2 className="relative text-xl font-bold">
-      <span className="absolute z-[-1] top-[25%] translate-y-[-50%] md:translate-x-[-50%] text-7xl font-serif bg-clip-text text-transparent bg-primary-1 saturate-0 brightness-10">
+    <h2 className="relative text-xl font-bold font-serif">
+      <span className="absolute z-[-1] top-[25%] translate-y-[-50%] md:translate-x-[-50%] text-7xl bg-clip-text text-transparent bg-primary-1 saturate-0 brightness-10">
         {roman[number - 1]}
       </span>
-      <span className="text-xl static text-primary-1 brightness-75 saturate-75">
+      <span className="text-2xl static text-primary-1 brightness-75 saturate-75">
         {content}
       </span>
     </h2>
