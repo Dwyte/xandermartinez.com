1f6c708b4913c001d4656faa8614982d3dd5cf1c
Xander Dwight <martindwyte@gmail.com>
2025-10-01 13:50:53 +0800

    style: apply primary color on numbered-header

--- app/components/numbered-header/numbered-header.tsx
+++ app/components/numbered-header/numbered-header.tsx
@@ -6,10 +6,10 @@ type NumberedHeaderProps = {
 export function NumberedHeader({ number, content }: NumberedHeaderProps) {
   return (
     <h2 className="relative text-xl font-bold">
-      <span className="absolute z-[-1] left-[-42] top-[25%] translate-[-50%] translate-x-[-50%] text-7xl font-mono bg-clip-text text-transparent bg-linear-to-l from-gray-900 via-50% via-gray-900/30 to-gray-900/0">
+      <span className="absolute z-[-1] left-[-42] top-[25%] translate-[-50%] translate-x-[-50%] text-7xl font-mono bg-clip-text text-transparent bg-linear-to-l from-primary-1 via-50% via-gray-primary-1/30 to-primary-1/0 saturate-0 brightness-10">
         {number.toString(2).padStart(4, "0")}
       </span>
-      <span className="text-xl static text-gray-700 brightness-125">
+      <span className="text-xl static text-primary-1 brightness-50">
         {content}
       </span>
     </h2>
